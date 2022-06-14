95462d1650cc00253369ea616bc6beae739c1c24
Daniyar <yerkinov.daniyar@yandex.kz>
2022-06-14 18:27:02 +0600

    deploying reactapp to gh pages

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="wrapper clear">
       <header className="d-flex justify-between align-center p-40">
         <div className = "d-flex align-center" >
-        <img width={40} height={40} src="/img/logo.png"/>
+        <img width={40} height={40} src="/img/logo.png"></img>
         <div>
           <h3 className="text-uppercase">React Sneakers</h3>
           <p className="opacity-5">Магазин лучших кросовок</p>
